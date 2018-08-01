50414f16a93c12ae0940f86328b4cdbc6787bbaa
Eli <elivining@gmail.com>
2018-08-01 08:53:03 -0500

    Add project files.

---
+++ connectingToADatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace connectingToADatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
